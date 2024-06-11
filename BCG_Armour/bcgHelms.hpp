b75438a6042b91d8a13ddbe5904689c69e18bb23
TheIndiv <faizanquddoos@gmail.com>
2024-06-11 14:19:04 +0500

    1

--- BCG_Armour/bcgHelms.hpp
+++ BCG_Armour/bcgHelms.hpp
@@ -1,53 +1,3 @@
-class BCG_Helmet_Guard: LST_Clone_Helmet_Guard 
-	{
-		displayName = "[BCG] Guard Helmet";
-		hiddenSelections[] = {
-			"camo1",
-			"camo2"
-		};
-		hiddenSelectionsTextures[] = {
-			"Clone_Armor_Unit\data\Helemt_Guard_ca.paa",
-			"Clone_Armor_Unit\data\Helemt_Guard_ca.paa"
-		};
-	};
-	class BCG_Helmet_Commando: LST_Merc_Helmet_Ver {
-		displayName = "[BCG] Commando Helmet";
-		hiddenSelections[] = {
-			"Camo1"
-		};
-		hiddenSelectionsMaterials[] = {
-			"Last_Force_Unit_1\Merc\data\Ver_helmet.rvmat"
-		};
-		hiddenSelectionsTextures[] = {
-			"Last_Force_Unit_1\Merc\data\Ver_helmet_co.paa"
-		};
-	};
-	class BCG_Helmet_Gunslinger: ls_mandalorian_dinDjarin_helmet {
-		displayName = "[BCG] Gunslinger Helmet";
-		hiddenSelections[] = {
-			"camo1",
-			"visor",
-			"neckTex"
-		};
-		hiddenSelectionsTextures[] = {
-			"ls_armor_greenfor\helmet\mandalorian\dinDjarin\data\helmet_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\dinDjarin\data\visor_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\dinDjarin\data\neck_co.paa"
-		};
-	};
-	class BCG_Helmet_Executioner: ls_mandalorian_executioner_helmet {
-		displayName = "[BCG] Executioner Helmet";
-		hiddenSelections[] = {
-			"camo1",
-			"visor",
-			"neckTex"
-		};
-		hiddenSelectionsTextures[] = {
-			"ls_armor_greenfor\helmet\mandalorian\executioner\data\helmet_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\executioner\data\visor_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\executioner\data\neck_co.paa"
-		};
-	};
 	class BCG_Helmet_Armorer: ls_mandalorian_armorer_helmet {
 		displayName = "[BCG] Armorer Helmet";
 		hiddenSelections[] = {
@@ -55,74 +5,116 @@ class BCG_Helmet_Guard: LST_Clone_Helmet_Guard
 			"visor",
 			"neckTex"
 		};
+		hiddenSelectionsMaterials[] = {
+			"BCG_Armour\data\helmets\Armorer\helmet_beskar.rvmat",
+			"BCG_Armour\data\helmets\Armorer\visor.rvmat",
+			"BCG_Armour\data\helmets\Armorer\neck.rvmat"
+		};
 		hiddenSelectionsTextures[] = {
-			"ls_armor_greenfor\helmet\mandalorian\armorer\data\helmet_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\armorer\data\visor_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\armorer\data\visor_co.paa"
+			"BCG_Armour\data\helmets\Armorer\helmet_co.paa",
+			"BCG_Armour\data\helmets\Armorer\visor_co.paa",
+			"BCG_Armour\data\helmets\Armorer\visor_co.paa"
 		};
 	};
-	class BCG_Helmet_Traditional: ls_mandalorian_traditional_helmet {
-		displayName = "[BCG] Traditional Helmet";
+	
+	class BCG_Helmet_Commando: LST_Merc_Helmet_Ver {
+		displayName = "[BCG] Commando Helmet";
+		hiddenSelections[] = {
+			"Camo1"
+		};
+		hiddenSelectionsMaterials[] = {
+			//"Last_Force_Unit_1\Merc\data\Ver_helmet.rvmat"
+			"BCG_Armour\data\helmets\Commando\Ver_helmet.rvmat"
+		};
+		hiddenSelectionsTextures[] = {
+			"BCG_Armour\data\helmets\Commando\Ver_helmet_co.paa"
+		};
+	};
+	
+	class BCG_Helmet_Executioner: ls_mandalorian_executioner_helmet {
+		displayName = "[BCG] Executioner Helmet";
 		hiddenSelections[] = {
 			"camo1",
 			"visor",
 			"neckTex"
 		};
+		hiddenSelectionsMaterials[] = {
+			"BCG_Armour\data\helmets\Executioner\helmet.rvmat",
+			"BCG_Armour\data\helmets\Executioner\visor.rvmat",
+			"BCG_Armour\data\helmets\Executioner\neck.rvmat"
+		};
 		hiddenSelectionsTextures[] = {
-			"ls_armor_greenfor\helmet\mandalorian\traditional\data\helmet_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\traditional\data\visor_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\traditional\data\neck_co.paa"
+			"BCG_Armour\data\helmets\Executioner\helmet_co.paa",
+			"BCG_Armour\data\helmets\Executioner\visor_co.paa",
+			"BCG_Armour\data\helmets\Executioner\neck_co.paa"
 		};
 	};
+	
+	class BCG_Helmet_Guard: LST_Clone_Helmet_Guard {
+		displayName = "[BCG] Guard Helmet";
+		hiddenSelections[] = {
+			"camo1",
+			"camo2"
+		};
+		hiddenSelectionsMaterials[] = {
+			"BCG_Armour\data\helmets\shiny.rvmat",
+			"BCG_Armour\data\helmets\shiny.rvmat"
+		};
+		hiddenSelectionsTextures[] = {
+			"BCG_Armour\data\helmets\Guard\Helemt_Guard_ca.paa",
+			"BCG_Armour\data\helmets\Guard\Helemt_Guard_ca.paa"
+		};
+	};
+	
+	class BCG_Helmet_Gunslinger: ls_mandalorian_dinDjarin_helmet {
+		displayName = "[BCG] Gunslinger Helmet";
+		hiddenSelections[] = {
+			"camo1",
+			"visor",
+			"neckTex"
+		};
+		hiddenSelectionsMaterials[] = {
+			"BCG_Armour\data\helmets\Gunslinger\helmet_beskar.rvmat",
+			"BCG_Armour\data\helmets\Gunslinger\visor.rvmat",
+			"BCG_Armour\data\helmets\Gunslinger\neck.rvmat"
+		};
+		hiddenSelectionsTextures[] = {
+			"BCG_Armour\data\helmets\Gunslinger\helmet_co.paa",
+			"BCG_Armour\data\helmets\Gunslinger\visor_co.paa",
+			"BCG_Armour\data\helmets\Gunslinger\neck_co.paa"
+		};
+	};
+	
+	class BCG_Helmet_Heavy: Merc_Helmet_Hyperion {
+		displayName = "[BCG] Heavy Helmet";
+		hiddenSelections[] = {
+			"Camo1"
+		};
+		hiddenSelectionsMaterials[] = {
+			//"armor_unit\Hyperion\Helmet_maze.rvmat"
+			"BCG_Armour\data\helmets\Heavy\Helmet_maze.rvmat"
+		};
+		hiddenSelectionsTextures[] = {
+			"BCG_Armour\data\helmets\Heavy\Hyperion_helmet_co.paa"
+		};
+	};
+	
 	class BCG_Helmet_Imperial: LST_merc_helmet_v1_free {
 		displayName = "[BCG] Imperial Helmet";
 		hiddenSelections[] = {
 			"camo1",
 			"visor"
 		};
-		hiddenSelectionsTextures[] = {
-			"Last_Force_Unit_1\Merc\Helmet_FR.paa",
-			"Last_Force_Unit_1\Merc\Helmet_FR.paa"
-		};
-	};
-	class BCG_Helmet_SplitJaw: ls_mandalorian_elite_helmet {
-		displayName = "[BCG] Split Jaw Helmet";
-		hiddenSelections[] = {
-			"camo1",
-			"visor",
-			"neckTex"
-		};
-		hiddenSelectionsTextures[] = {
-			"ls_armor_greenfor\helmet\mandalorian\elite\data\helmet_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\elite\data\visor_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\elite\data\neck_co.paa"
-		};
-	};
-	class BCG_Helmet_Stalker: ls_mandalorian_stalker_helmet {
-		displayName = "[BCG] Stalker Helmet";
-		hiddenSelections[] = {
-			"camo1",
-			"visor",
-			"neckTex"
-		};
-		hiddenSelectionsTextures[] = {
-			"ls_armor_greenfor\helmet\mandalorian\stalker\data\helmet_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\stalker\data\visor_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\stalker\data\neck_co.paa"
-		};
-	};
-	class BCG_Helmet_Notre: LST_Mando_G_Notre_Helmet {
-		displayName = "[BCG] Notre Helmet";
-		hiddenSelections[] = {"Camo1","visor"};
 		hiddenSelectionsMaterials[] = {
-			"Last_Force_Unit_1\Merc\G_Notre_Helmet.rvmat",
-			"Last_Force_Unit_1\Merc\G_Visor_Notre_Helmet.rvmat"
+			"BCG_Armour\data\helmets\shiny.rvmat",
+			"BCG_Armour\data\helmets\shiny.rvmat"
 		};
 		hiddenSelectionsTextures[] = {
-			"Last_Force_Unit_1\Merc\G_Notre_Helmet_co.paa",
-			"Last_Force_Unit_1\Merc\G_Notre_Helmet_co.paa"
+			"BCG_Armour\data\helmets\Imperial\Helmet_FR.paa",
+			"BCG_Armour\data\helmets\Imperial\Helmet_FR.paa"
 		};
 	};
+	
 	class BCG_Helmet_Nightowl: ls_mandalorian_nightowl_helmet {
 		displayName = "[BCG] Nightowl Helmet";
 		hiddenSelections[] = {
@@ -130,39 +122,31 @@ class BCG_Helmet_Guard: LST_Clone_Helmet_Guard
 			"visor",
 			"neckTex"
 		};
-		hiddenSelectionsTextures[] = {
-			"ls_armor_greenfor\helmet\mandalorian\nightowl\data\helmet_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\nightowl\data\visor_co.paa",
-			"ls_armor_greenfor\helmet\mandalorian\nightowl\data\neck_co.paa"
-		};
-	};
-	class BCG_Helmet_Heavy: Merc_Helmet_Hyperion {
-		displayName = "[BCG] Heavy Helmet";
-		hiddenSelections[] = {
-			"Camo1"
-		};
 		hiddenSelectionsMaterials[] = {
-			"armor_unit\Hyperion\Helmet_maze.rvmat"
+			"BCG_Armour\data\helmets\Nightowl\helmet.rvmat",
+			"BCG_Armour\data\helmets\Nightowl\visor.rvmat",
+			"BCG_Armour\data\helmets\Nightowl\neck.rvmat"
 		};
 		hiddenSelectionsTextures[] = {
-			"\BCG_Armour\data\helmets\Hyperion_helmet_co.paa"
+			"BCG_Armour\data\helmets\Nightowl\helmet_co.paa",
+			"BCG_Armour\data\helmets\Nightowl\visor_co.paa",
+			"BCG_Armour\data\helmets\Nightowl\neck_co.paa"
 		};
 	};
-	class BCG_Helmet_Recruit: LST_Mando_G_cyclo_helmet {
-		displayName = "[BCG] Recruit Helmet";
-		hiddenSelections[] = {
-			"Camo1",
-			"visor"
-		};
+	
+	class BCG_Helmet_Notre: LST_Mando_G_Notre_Helmet {
+		displayName = "[BCG] Notre Helmet";
+		hiddenSelections[] = {"Camo1","visor"};
 		hiddenSelectionsMaterials[] = {
-			"Last_Force_Unit_1\Merc\Mando_Cyclo_helmet.rvmat",
-			"Last_Force_Unit_1\Merc\Visor_Mando.rvmat"
+			"BCG_Armour\data\helmets\Notre\G_Notre_Helmet.rvmat",
+			"BCG_Armour\data\helmets\Notre\G_Visor_Notre_Helmet.rvmat"
 		};
 		hiddenSelectionsTextures[] = {
-			"Last_Force_Unit_1\Merc\G_Cyclo_helmet_co.paa",
-			"Last_Force_Unit_1\Merc\G_Cyclo_helmet_co.paa"
+			"BCG_Armour\data\helmets\Notre\G_Notre_Helmet_co.paa",
+			"BCG_Armour\data\helmets\Notre\G_Notre_Helmet_co.paa"
 		};
 	};
+	
 	class BCG_Helmet_Pilot: LST_Mando_G_Fil_cyclo_helmet {
 		displayName = "[BCG] Pilot Helmet";
 		hiddenSelections[] = {
@@ -170,7 +154,84 @@ class BCG_Helmet_Guard: LST_Clone_Helmet_Guard
 			"visor"
 		};
 		hiddenSelectionsMaterials[] = {
-			"Last_Force_Unit_1\Merc\Mando_Cyclo_Fil_helmet.rvmat",
-			"Last_Force_Unit_1\Merc\Visor_Fil_Mando.rvmat"
+			"BCG_Armour\data\helmets\Pilot\Mando_Cyclo_Fil_helmet.rvmat",
+			"BCG_Armour\data\helmets\Pilot\Visor_Fil_Mando.rvmat"
+		};
+		hiddenSelectionsTextures[] = {
+			"BCG_Armour\data\helmets\Pilot\G_Cyclo_helmet_Fil_co.paa",
+			"BCG_Armour\data\helmets\Pilot\G_Cyclo_helmet_Fil_co.paa"
+		};
+	};
+	
+	class BCG_Helmet_Recruit: LST_Mando_G_cyclo_helmet {
+		displayName = "[BCG] Recruit Helmet";
+		hiddenSelections[] = {
+			"Camo1",
+			"visor"
+		};
+		hiddenSelectionsMaterials[] = {
+			"BCG_Armour\data\helmets\Recruit\Mando_Cyclo_helmet.rvmat",
+			"BCG_Armour\data\helmets\Recruit\Visor_Mando.rvmat"
+		};
+		hiddenSelectionsTextures[] = {
+			"BCG_Armour\data\helmets\Recruit\G_Cyclo_helmet_co.paa",
+			"BCG_Armour\data\helmets\Recruit\G_Cyclo_helmet_co.paa"
+		};
+	};
+	
+	class BCG_Helmet_SplitJaw: ls_mandalorian_elite_helmet {
+		displayName = "[BCG] Split Jaw Helmet";
+		hiddenSelections[] = {
+			"camo1",
+			"visor",
+			"neckTex"
+		};
+		hiddenSelectionsMaterials[] = {
+			"BCG_Armour\data\helmets\SplitJaw\helmet.rvmat",
+			"BCG_Armour\data\helmets\SplitJaw\visor.rvmat",
+			"BCG_Armour\data\helmets\SplitJaw\neck.rvmat"
+		};
+		hiddenSelectionsTextures[] = {
+			"BCG_Armour\data\helmets\SplitJaw\helmet_co.paa",
+			"BCG_Armour\data\helmets\SplitJaw\visor_co.paa",
+			"BCG_Armour\data\helmets\SplitJaw\neck_co.paa"
+		};
+	};
+	
+	class BCG_Helmet_Stalker: ls_mandalorian_stalker_helmet {
+		displayName = "[BCG] Stalker Helmet";
+		hiddenSelections[] = {
+			"camo1",
+			"visor",
+			"neckTex"
+		};
+		hiddenSelectionsMaterials[] = {
+			"BCG_Armour\data\helmets\Stalker\helmet.rvmat",
+			"BCG_Armour\data\helmets\Stalker\visor.rvmat",
+			"BCG_Armour\data\helmets\Stalker\neck.rvmat"
+		};
+		hiddenSelectionsTextures[] = {
+			"BCG_Armour\data\helmets\Stalker\helmet_co.paa",
+			"BCG_Armour\data\helmets\Stalker\visor_co.paa",
+			"BCG_Armour\data\helmets\Stalker\neck_co.paa"
+		};
+	};
+	
+	class BCG_Helmet_Traditional: ls_mandalorian_traditional_helmet {
+		displayName = "[BCG] Traditional Helmet";
+		hiddenSelections[] = {
+			"camo1",
+			"visor",
+			"neckTex"
+		};
+		hiddenSelectionsMaterials[] = {
+			"BCG_Armour\data\helmets\Traditional\helmet.rvmat",
+			"BCG_Armour\data\helmets\Traditional\visor.rvmat",
+			"BCG_Armour\data\helmets\Traditional\neck.rvmat"
+		};
+		hiddenSelectionsTextures[] = {
+			"ls_armor_greenfor\helmet\mandalorian\traditional\data\helmet_co.paa",
+			"ls_armor_greenfor\helmet\mandalorian\traditional\data\visor_co.paa",
+			"ls_armor_greenfor\helmet\mandalorian\traditional\data\neck_co.paa"
 		};
 	};
