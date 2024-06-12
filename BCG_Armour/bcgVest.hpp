da3391c80543882c122cfd899ceb8b717621360f
Neiferr <joshnofer333@gmail.com>
2024-06-12 19:48:03 -0400

    Vests done for now
    jorking my peanits

---
+++ BCG_Armour/bcgVest.hpp
@@ -0,0 +1,148 @@
+	class BCG_Kama: SFA_Kama
+	{
+		scope=2;
+		scopeArsenal=2;
+		displayName = "[BCG] Kama";
+		model = "SFA_Equipment_R\Vest\kama.p3d";
+		hiddenSelections[] = {
+			"camo1"
+		};
+		hiddenSelectionsTextures[] = {
+			"SFA_Equipment_R\Vest\data\kama_blnk_co.paa"
+		};
+		class ItemInfo: VestItem
+		{
+			vestType = "Rebreather";
+			uniformModel = "SFA_Equipment_R\Vest\kama.p3d";
+			containerClass = "Supply100";
+			mass = 80;
+			modelSides[]={6};
+			hiddenSelections[] = {
+				"camo1"
+			};
+			hiddenSelectionsTextures[] = {
+				"SFA_Equipment_R\Vest\data\kama_blnk_co.paa"
+			
+			};
+		};
+	};
+	
+	class BCG_Kama_Ammo_Sash :BCG_Kama
+	{
+		scope=2;
+		scopeArsenal=2;
+		displayName = "[BCG] Kama & Sash";
+		model = "SFA_Equipment_R\Vest\Ammo_Sash_NCO.p3d";
+		hiddenSelections[] = {
+			"camo1",
+			"camo2"
+		};
+		hiddenSelectionsTextures[] = {
+			"SFA_Equipment_R\Vest\data\Sash_co.paa",
+			"SFA_Equipment_R\Vest\data\kama_STND_co.paa"
+		};
+		class ItemInfo: VestItem
+		{
+			vestType = "Rebreather";
+			uniformModel = "SFA_Equipment_R\Vest\Ammo_Sash_NCO.p3d";
+			containerClass = "Supply100";
+			mass = 80;
+			modelSides[]={6};
+			hiddenSelections[] = {
+				"camo1",
+				"camo2"
+			};
+			hiddenSelectionsTextures[] = {
+				"SFA_Equipment_R\Vest\data\Sash_co.paa",
+				"SFA_Equipment_R\Vest\data\kama_STND_co.paa"
+			
+				};
+			};
+		};
+		
+		class BCG_Ammo_Sash :BCG_Kama
+	{
+		scope=2;
+		scopeArsenal=2;
+		displayName = "[BCG] Ammo Sash";
+		model = "SFA_Equipment_R\Vest\Ammo_Sash.p3d";
+		hiddenSelections[] = {
+			"camo1"
+		};
+		hiddenSelectionsTextures[] = {
+			"SFA_Equipment_R\Vest\data\Sash_co.paa"
+		};
+		class ItemInfo: VestItem
+		{
+			vestType = "Rebreather";
+			uniformModel = "SFA_Equipment_R\Vest\Ammo_Sash.p3d";
+			containerClass = "Supply100";
+			mass = 80;
+			modelSides[]={6};
+			hiddenSelections[] = {
+				"camo1"
+			};
+			hiddenSelectionsTextures[] = {
+				"SFA_Equipment_R\Vest\data\Sash_co.paa"	
+			
+				};
+			};
+		};
+		
+		class BCG_Pauldron : BCG_Kama
+	{
+		scope=2;
+		scopeArsenal=2;
+		displayName = "[BCG] Pauldron";
+		model = "Clone_Armor_Unit\Clone_Trooper_Vest_Up.p3d"
+		hiddenSelections[] = {
+			"camo1"
+		};
+		hiddenSelectionsTextures[] = {
+			"Clone_Armor_Unit\data\Clone_ARC_Vest_co.paa"
+		};
+		class ItemInfo: VestItem
+		{
+			vestType = "Rebreather";
+			uniformModel = "Clone_Armor_Unit\Clone_Trooper_Vest_Up.p3d";
+			containerClass = "Supply100";
+			mass = 80;
+			modelSides[]={6};
+			hiddenSelections[] = {
+				"camo1"
+			};
+			hiddenSelectionsTextures[] = {
+				"Clone_Armor_Unit\data\Clone_ARC_Vest_co.paa"	
+			
+				};
+			};
+		};
+		
+		class BCG_Pauldron_Holster : BCG_Kama
+	{	
+		scope=2;
+		scopeArsenal=2;
+		displayName = "[BCG] Pauldron & Holsters";
+		model = "Clone_Armor_Unit\Clone_Trooper_Vest_Up_Holster.p3d"
+		hiddenSelections[] = {
+			"camo1"
+		};
+		hiddenSelectionsTextures[] = {
+			"Clone_Armor_Unit\data\Clone_ARC_Vest_co.paa"
+		};
+		class ItemInfo: VestItem
+		{
+			vestType = "Rebreather";
+			uniformModel = "Clone_Armor_Unit\Clone_Trooper_Vest_Up_Holster.p3d";
+			containerClass = "Supply100";
+			mass = 80;
+			modelSides[]={6};
+			hiddenSelections[] = {
+				"camo1"
+			};
+			hiddenSelectionsTextures[] = {
+				"Clone_Armor_Unit\data\Clone_ARC_Vest_co.paa"	
+			
+				};
+			};
+		};
