dd374c7fddfaca5461c5ae3acec47da42542aec3
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2024-06-15 20:31:21 -0700

    added weapons folder

---
+++ BCG_Weapons/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+	class BCG_Weapons
+	{
+		requiredAddons[] 	= {"OPTRE_Weapons"};
+		requiredVersion=0.1;
+		units[]={};
+		weapons[]={};
+		authors[]={"Gray","Neifer","Indie Beninging"};
+	};
+};
+class cfgWeapons {
+	class OPTRE_CQS48_Bulldog_Automatic;
+	
+	
+};
