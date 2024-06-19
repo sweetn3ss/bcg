78354d7f1824f2f7cdc647cf303f1e33b06baaa8
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2024-06-19 13:46:36 -0700

    dullbog and stewart
    gun

--- BCG_Weapons/config.cpp
+++ BCG_Weapons/config.cpp
@@ -2,7 +2,7 @@ class CfgPatches
 {
 	class BCG_Weapons
 	{
-		requiredAddons[] 	= {"OPTRE_Weapons"};
+		requiredAddons[]={"OPTRE_Weapons"};
 		requiredVersion=0.1;
 		units[]={};
 		weapons[]={};
@@ -11,20 +11,26 @@ class CfgPatches
 };
 class cfgWeapons {
 	class OPTRE_CQS48_Bulldog_Automatic;
+	class SWLW_Westar35S;
 	
-	class BCG_Bulldog: Optre_CQS48_Bulldog_Automatic
-	
-	{
-		displayName = "BCG_Bulldog";
-		BaseWeapon = "BCG_Bulldog";
-		
-		hiddenSelectionsTextures[] = {
-			"BCG_Weapons\dullbog\Body_Black.paa";
-			"BCG_wEapons\dullbog\data\\DecalSheet_co.paa"
+	class BCG_Bulldog: OPTRE_CQS48_Bulldog_Automatic {
+		displayName="[BCG] Bulldog Slugthrower";
+		baseWeapon="BCG_Bulldog";
+		hiddenSelections[]={
+			"camoBody",
+			"camoDecal"
+		};
+		hiddenSelectionsTextures[]={
+			"BCG_Weapons\dullbog\Body_Black.paa",
+			"BCG_Weapons\dullbog\DecalSheet_co.paa"
 		}; 
-		hiddenSelectionsMaterials[] = {
-			"BCG_Weapons\dullbog\body.rvmat";
+		hiddenSelectionsMaterials[]={
+			"BCG_Weapons\dullbog\body.rvmat",
 			"BCG_Weapons\dullbog\decal.rvmat"
-			
-			
+		};
+	};
+	class BCG_Westar35S: SWLW_Westar35S {
+		displayName="[BCG] Westar-35S";
+		baseWeapon="BCG_Westar35S";
+	};
 };
