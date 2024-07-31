3a83a267ad047bb13ac7b8291708afd5c4b79831
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2024-07-31 00:30:53 -0700

    inclusion of BCG_Functions
    added medical controller scripts for tag and attack injuries

---
+++ BCG_Functions/config.cpp
@@ -0,0 +1,18 @@
+class cfgPatches {
+	class BCG_Functions {
+		requiredVersion = 0.1;
+		requiredAddons []= {"BCG_Core"};
+		units[]={};
+		weapons[]={};
+	};
+};
+class cfgFunctions {
+	class bcg_medbay {
+		class functions {
+			file = "BCG_Functions\functions";
+			class injuryCase {};
+			class injuryConsole {};
+			class injuryAssign {};
+		};
+	};
+};
