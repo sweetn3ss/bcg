ef125b569f9f2a0322b3ea2625821e7b3c072f3c
Neiferr <joshnofer333@gmail.com>
2024-08-03 02:49:57 -0400

    GYATTTTTTTTT

--- BCG_Vehicles/config.cpp
+++ BCG_Vehicles/config.cpp
@@ -24,8 +24,8 @@ class cfgVehicles {
 		};
 		textureList[]={
 			"buycegal",1,
-			"neif",1,
-			"balls",1
+			"neif",0,
+			"balls",0
 		};
 		class textureSources {
 			class buycegal
@@ -33,9 +33,9 @@ class cfgVehicles {
 				displayName = "Buy'ce Gal Cabur";
 				author = "your mom";
 				textures[] = { 
-					"\knd_fang\data\fuselage_co.paa",
-					"\knd_fang\data\wings_co.paa",
-					"\knd_fang\data\cockpit_co.paa"
+					"BCG_Vehicles\fang\bussygal\fuselage_co.paa",
+					"BCG_Vehicles\fang\bussygal\wings_co.paa",
+					"BCG_Vehicles\fang\bussygal\cockpit_co.paa"
 				};
 				factions[]={};
 			};
@@ -43,9 +43,9 @@ class cfgVehicles {
 			{
 				displayName = "Neifer";
 				textures[] = { 
-					"\knd_fang\data\fuselage_co.paa",
-					"\knd_fang\data\wings_co.paa",
-					"\knd_fang\data\cockpit_co.paa"
+					"BCG_Vehicles\fang\chrome\fuselage_co.paa",
+					"BCG_Vehicles\fang\chrome\wings_co.paa",
+					"BCG_Vehicles\fang\chrome\cockpit_co.paa"
 				};
 			};
 			class balls: buycegal
@@ -57,6 +57,26 @@ class cfgVehicles {
 					"\knd_fang\data\cockpit_co.paa"
 				};
 			};
+		};
+	};
+	class BCG_Fang_chromedome: BCG_Fang_NeiferTesticles {
+		displayName = "[BCG] Shiny Fang-class Fighter";
+		scope = 2;
+		scopeArsenal = 2;
+		hiddenSelections[] = {
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsMaterials[]={
+			"BCG_Vehicles\fang\chrome\Fuselage.rvmat",
+			"BCG_Vehicles\fang\chrome\Wings.rvmat",
+			"BCG_Vehicles\fang\chrome\Cockpit"
+		};
+		hiddenSelectionsTextures[] = {
+			"BCG_Vehicles\fang\chrome\fuselage_co.paa",
+			"BCG_Vehicles\fang\chrome\wings_co.paa",
+			"BCG_Vehicles\fang\chrome\cockpit_co.paa"
 		};
 	};
 };
