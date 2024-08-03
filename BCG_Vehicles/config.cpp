963a21960746ec75973052da4b3180c2e5de5020
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2024-08-02 23:13:15 -0700

    try now lmao
    uwu

--- BCG_Vehicles/config.cpp
+++ BCG_Vehicles/config.cpp
@@ -56,6 +56,38 @@ class cfgVehicles {
 					"\knd_fang\data\wings_co.paa",
 					"\knd_fang\data\cockpit_co.paa"
 				};
+=======
+			"neif",0,
+			"balls",0
+		};
+		class buycegal
+		{
+			displayName = "[BCG] Factory Paint";
+			author = "your mom";
+			textures[] = { 
+				"BCG_Vehicles\fang\fuselage_co.paa",
+				"BCG_Vehicles\fang\wings_co.paa",
+				"BCG_Vehicles\fang\cockpit_co.paa"
+			};
+			factions[]={};
+		};
+		class neif: buycegal
+		{
+			displayName = "ooh shiny";
+			textures[] = { 
+				"\knd_fang\data\fuselage_co.paa",
+				"\knd_fang\data\wings_co.paa",
+				"\knd_fang\data\cockpit_co.paa"
+			};
+		};
+		class balls: buycegal
+		{
+			displayName = "Night Fighter";
+			textures[] = { 
+				"\knd_fang\data\fuselage_co.paa",
+				"\knd_fang\data\wings_co.paa",
+				"\knd_fang\data\cockpit_co.paa"
+>>>>>>> a652981c34cb64be3e5b323223e72ef6db8fd6ae
 			};
 		};
 	};
