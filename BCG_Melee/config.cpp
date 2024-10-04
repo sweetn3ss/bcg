aeff417239a9c052b7aea841cf5d1ea7b2b15291
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2024-10-03 18:12:32 -0700

    melee being funky but it exists now

---
+++ BCG_Melee/config.cpp
@@ -0,0 +1,24 @@
+class cfgPatches {
+	class BCG_Melee {
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={
+			"BCG_Core",
+			"A3_Weapons_F",
+			"WBK_NewMeleeWEaponsTier2"
+		};
+	};
+};
+class cfgWeapons {
+	class WBK_survival_weapon_2;
+	class BCG_Thunderhammer: WBK_survival_weapon_2
+	{
+		IMS_WeaponType = "Heavy";
+		IMS_Melee_Param_Sounds = "[""generis_empty_1"",""generis_empty_2"",""generis_empty_3"",""generis_empty_4"",""generis_empty_5""]";
+		IMS_Melee_Param_Damage = 0.8;
+		model = "BCG_Melee\data\thunderhammer\thunderhammer.p3d";
+		displayName = "[BCG] Shuk'Orok";
+		descriptionShort = "Forged with 40,000 souls.";
+	};
+};
