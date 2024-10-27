24fc9359fd93857c4a927ce6076a2abc47a9b853
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2024-10-26 23:02:35 -0700

    Revert "cruncheth"
    This reverts commit 7bddb4e2e9a0b757004af1337a0f96dba73cef18.

--- BCG_Melee/config.cpp
+++ BCG_Melee/config.cpp
@@ -14,12 +14,12 @@ class cfgWeapons {
 	class WBK_survival_weapon_2;
 	class BCG_Thunderhammer: WBK_survival_weapon_2
 	{
-		IMS_WeaponType="Heavy";
-		IMS_Melee_Param_Sounds="[""generis_empty_1"",""generis_empty_2"",""generis_empty_3"",""generis_empty_4"",""generis_empty_5""]";
-		IMS_Melee_Param_Damage=0.8;
-		model="BCG_Melee\data\thunderhammer\thunderhammer.p3d";
-		displayName="[BCG] Shuk'Orok";
-		descriptionShort="Forged from 40,000 souls.";
+		IMS_WeaponType = "Heavy";
+		IMS_Melee_Param_Sounds = "[""generis_empty_1"",""generis_empty_2"",""generis_empty_3"",""generis_empty_4"",""generis_empty_5""]";
+		IMS_Melee_Param_Damage = 0.8;
+		model = "BCG_Melee\data\thunderhammer\thunderhammer.p3d";
+		displayName = "[BCG] Shuk'Orok";
+		descriptionShort = "Forged from 40,000 souls.";
 		hiddenSelections[]={"camo1","camo2"};
 		hiddenSelectionsTextures[]={
 			"BCG_Melee\data\thunderhammer\camo1_CO.paa",
@@ -28,13 +28,15 @@ class cfgWeapons {
 	};
 	class BCG_LegoSpear: WBK_survival_weapon_2
 	{
-		IMS_WeaponType="Poleax";
-		IMS_Melee_Param_Sounds="[""generis_empty_1"",""generis_empty_2"",""generis_empty_3"",""generis_empty_4"",""generis_empty_5""]";
-		IMS_Melee_Param_Damage=0.5;
-		model="BCG_Melee\data\beskar\spear.p3d";
-		displayName="[BCG] Beviiragir";
-		descriptionShort="Hecho en China";
+		IMS_WeaponType = "Poleax";
+		IMS_Melee_Param_Sounds = "[""generis_empty_1"",""generis_empty_2"",""generis_empty_3"",""generis_empty_4"",""generis_empty_5""]";
+		IMS_Melee_Param_Damage = 0.5;
+		model = "BCG_Melee\data\beskar\spear.p3d";
+		displayName = "[BCG] Beviiragir";
+		descriptionShort = "The Danes cannot comprehend such comedy.";
 		hiddenSelections[]={"camo"};
-		hiddenSelectionsTextures[]={"BCG_Melee\data\beskar\camo_CO.paa"};
+		hiddenSelectionsTextures[]={
+			"BCG_Melee\data\beskar\camo_CO.paa",
+		};
 	};
 };
