afe8253c332b8ffe7ec91187ee2f67dc2425743f
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2024-10-26 22:31:43 -0700

    line crunch and name fix

--- BCG_Facewear/config.cpp
+++ BCG_Facewear/config.cpp
@@ -1,7 +1,7 @@
 class cfgPatches {
 	class BCG_Facewear {
 		authors[]={"Gray","Neifer","Indie Beninging"};
-		name="Buy'ce Gal Cabur Core";
+		name="Buy'ce Gal Cabur Facewear";
 		units[]={};
 		weapons[]={};
 		requiredVersion=0.1;
@@ -40,15 +40,14 @@ class cfgWeapons {
 	class knd_nvg_nya;
 	class BCG_iPod_Beroya: knd_nvg_nya
 	{
-		scope = 2;
-		displayName = "[BCG] iPod (Beroya)";
-		model = "\BCG_Facewear\data\ipod\ipod.p3d";
-		hiddenSelections[] = {"camo1","camo2","camo3","camo4","dalverd","gen5"};
-		hiddenSelectionsTextures[] = {"\BCG_Facewear\data\ipod\camo1_co.paa","\BCG_Facewear\data\ipod\camo2_co.paa"};
-		picture = "\BCG_Facewear\data\iPod\icon.paa";
+		scope=2;
+		displayName="[BCG] iPod (Beroya)";
+		model="\BCG_Facewear\data\ipod\ipod.p3d";
+		hiddenSelections[]={"camo1","camo2","camo3","camo4","dalverd","gen5"};
+		hiddenSelectionsTextures[]={"\BCG_Facewear\data\ipod\camo1_co.paa","\BCG_Facewear\data\ipod\camo2_co.paa"};
+		picture="\BCG_Facewear\data\iPod\icon.paa";
 		class ItemInfo: ItemInfo {
-			hiddenSelections[]=
-			{
+			hiddenSelections[]={
 				"camo1",
 				"camo2",
 				"camo3",
@@ -56,20 +55,18 @@ class cfgWeapons {
 				"dalverd",
 				"gen5"
 			};
-			uniformModel = "\BCG_Facewear\data\ipod\ipod.p3d";
-			modelOff = "\BCG_Facewear\data\ipod\ipod.p3d";
+			uniformModel="\BCG_Facewear\data\ipod\ipod.p3d";
+			modelOff="\BCG_Facewear\data\ipod\ipod.p3d";
 		};
 	};
-	class BCG_iPod_Dalverd: BCG_iPod_Beroya
-	{
-		scope = 2;
-		displayName = "[BCG] iPod (Dalverd)";
-		model = "\BCG_Facewear\data\ipod\ipod.p3d";
-		hiddenSelections[] = {"camo1","camo2","camo3","camo4","beroya","gen5"};
-		hiddenSelectionsTextures[] = {"\BCG_Facewear\data\ipod\camo1_co.paa","\BCG_Facewear\data\ipod\camo2_co.paa"};
+	class BCG_iPod_Dalverd: BCG_iPod_Beroya {
+		scope=2;
+		displayName="[BCG] iPod (Dalverd)";
+		model="\BCG_Facewear\data\ipod\ipod.p3d";
+		hiddenSelections[]={"camo1","camo2","camo3","camo4","beroya","gen5"};
+		hiddenSelectionsTextures[]={"\BCG_Facewear\data\ipod\camo1_co.paa","\BCG_Facewear\data\ipod\camo2_co.paa"};
 		class ItemInfo: ItemInfo {
-			hiddenSelections[]=
-			{
+			hiddenSelections[]={
 				"camo1",
 				"camo2",
 				"camo3",
@@ -77,25 +74,23 @@ class cfgWeapons {
 				"beroya",
 				"gen5"
 			};
-			uniformModel = "\BCG_Facewear\data\ipod\ipod.p3d";
-			modelOff = "\BCG_Facewear\data\ipod\ipod.p3d";
+			uniformModel="\BCG_Facewear\data\ipod\ipod.p3d";
+			modelOff="\BCG_Facewear\data\ipod\ipod.p3d";
 		};
 	};
-	class BCG_iPod_Gen5: BCG_iPod_Beroya
-	{
-		scope = 2;
-		displayName = "[BCG] iPod Gen 5 (Beroya)";
-		model = "\BCG_Facewear\data\ipod\ipod.p3d";
-		hiddenSelections[] = {"camo1","camo2","camo3","camo4","beroya","dalverd"};
-		hiddenSelectionsTextures[] = {
+	class BCG_iPod_Gen5: BCG_iPod_Beroya {
+		scope=2;
+		displayName="[BCG] iPod Gen 5 (Beroya)";
+		model="\BCG_Facewear\data\ipod\ipod.p3d";
+		hiddenSelections[]={"camo1","camo2","camo3","camo4","beroya","dalverd"};
+		hiddenSelectionsTextures[]={
 			"\BCG_Facewear\data\ipod\camo1_co.paa",
 			"\BCG_Facewear\data\ipod\camo2_co.paa",
 			"\BCG_Facewear\data\ipod\camo3_co.paa",
 			"\BCG_Facewear\data\ipod\camo2_co.paa"
 		};
 		class ItemInfo: ItemInfo {
-			hiddenSelections[]=
-			{
+			hiddenSelections[]={
 				"camo1",
 				"camo2",
 				"camo3",
@@ -103,57 +98,54 @@ class cfgWeapons {
 				"beroya",
 				"dalverd"
 			};
-			uniformModel = "\BCG_Facewear\data\ipod\ipod.p3d";
-			modelOff = "\BCG_Facewear\data\ipod\ipod.p3d";
+			uniformModel="\BCG_Facewear\data\ipod\ipod.p3d";
+			modelOff="\BCG_Facewear\data\ipod\ipod.p3d";
 		};
 	};
 	
 	class BCG_Playlist_Yeet: ItemCore {
-		author = "Gray";
+		author="Gray";
 		scope=2;
 		scopeArsenal=2;
-		displayName = "[BCG] Playlist Yeet";
-		model = "\A3\weapons_F\ammo\mag_univ.p3d";
-		picture = "\BCG_Facewear\data\iPod\yeet2.paa";
-		detectRange = -1;
-		simulation = "ItemMineDetector";
-		useAsBinocular = 0;
-		type = 4096;
-		class ItemInfo: InventoryWeapon_Base_F
-		{
-			mass = 1;
+		displayName="[BCG] Playlist Yeet";
+		model="\A3\weapons_F\ammo\mag_univ.p3d";
+		picture="\BCG_Facewear\data\iPod\yeet2.paa";
+		detectRange=-1;
+		simulation="ItemMineDetector";
+		useAsBinocular=0;
+		type=4096;
+		class ItemInfo: InventoryWeapon_Base_F {
+			mass=1;
 		};
 	};
 	class BCG_Playlist_Ace: ItemCore {
-		author = "Gray";
+		author="Gray";
 		scope=2;
 		scopeArsenal=2;
-		displayName = "[BCG] Playlist Ace";
-		model = "\A3\weapons_F\ammo\mag_univ.p3d";
-		picture = "\BCG_Facewear\data\iPod\yeet2.paa";
-		detectRange = -1;
-		simulation = "ItemMineDetector";
-		useAsBinocular = 0;
-		type = 4096;
-		class ItemInfo: InventoryWeapon_Base_F
-		{
-			mass = 1;
+		displayName="[BCG] Playlist Ace";
+		model="\A3\weapons_F\ammo\mag_univ.p3d";
+		picture="\BCG_Facewear\data\iPod\yeet2.paa";
+		detectRange=-1;
+		simulation="ItemMineDetector";
+		useAsBinocular=0;
+		type=4096;
+		class ItemInfo: InventoryWeapon_Base_F {
+			mass=1;
 		};
 	};
 	class BCG_Playlist_Juggy: ItemCore {
-		author = "Gray";
+		author="Gray";
 		scope=2;
 		scopeArsenal=2;
-		displayName = "[BCG] Playlist Juggy";
-		model = "\A3\weapons_F\ammo\mag_univ.p3d";
-		picture = "\BCG_Facewear\data\BK\vatreicon2a.paa";
-		detectRange = -1;
-		simulation = "ItemMineDetector";
-		useAsBinocular = 0;
-		type = 4096;
-		class ItemInfo: InventoryWeapon_Base_F
-		{
-			mass = 1;
+		displayName="[BCG] Playlist Juggy";
+		model="\A3\weapons_F\ammo\mag_univ.p3d";
+		picture="\BCG_Facewear\data\BK\vatreicon2a.paa";
+		detectRange=-1;
+		simulation="ItemMineDetector";
+		useAsBinocular=0;
+		type=4096;
+		class ItemInfo: InventoryWeapon_Base_F {
+			mass=1;
 		};
 	};
 };
@@ -171,201 +163,137 @@ class cfgGlasses {
 	class G_Combat;
 	
 	
-	class BCG_arbiter_belt: tgf_facewear_arbiter_belt
-	{
+	class BCG_arbiter_belt: tgf_facewear_arbiter_belt {
 		scope=2;
 		scopeArsenal=2;
 		displayName="[BCG] Arbiter's Belt";
-		hiddenSelections[]=
-		{
-			"camo1"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"BCG_Facewear\data\arbiter\camo1_co.paa"
-		};
+		hiddenSelections[]={"camo1"};
+		hiddenSelectionsTextures[]={"BCG_Facewear\data\arbiter\camo1_co.paa"};
 	};
-	class BCG_forgemaster_belt: tgf_facewear_forgemaster_belt
-	{
+	class BCG_forgemaster_belt: tgf_facewear_forgemaster_belt {
 		scope=2;
 		scopeArsenal=2;
 		displayName="[BCG] Forgemaster's Belt";
-		hiddenSelections[]=
-		{
-			"camo1"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"BCG_Facewear\data\forge\camo1_co.paa"
-		};
+		hiddenSelections[]={"camo1"};
+		hiddenSelectionsTextures[]={"BCG_Facewear\data\forge\camo1_co.paa"};
 	};
-	class BCG_heavy_belt: tgf_facewear_heavy_belt
-	{
+	class BCG_heavy_belt: tgf_facewear_heavy_belt {
 		scope=2;
 		scopeArsenal=2;
 		displayName="[BCG] Heavy Belt";
-		hiddenSelections[]=
-		{
-			"camo1"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"BCG_Facewear\data\heavy\camo1_co.paa"
-		};
+		hiddenSelections[]={"camo1"};
+		hiddenSelectionsTextures[]={"BCG_Facewear\data\heavy\camo1_co.paa"};
 	};
-	class BCG_light_belt: tgf_facewear_light_belt
-	{
+	class BCG_light_belt: tgf_facewear_light_belt {
 		scope=2;
 		scopeArsenal=2;
 		displayName="[BCG] Light Belt";
-		hiddenSelections[]=
-		{
-			"camo1"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"BCG_Facewear\data\light\camo1_co.paa"
-		};
+		hiddenSelections[]={"camo1"};
+		hiddenSelectionsTextures[]={"BCG_Facewear\data\light\camo1_co.paa"};
 	};
-	class BCG_medium_belt: tgf_facewear_medium_belt
-	{
+	class BCG_medium_belt: tgf_facewear_medium_belt {
 		scope=2;
 		scopeArsenal=2;
 		displayName="[BCG] Medium Belt";
-		hiddenSelections[]=
-		{
-			"camo1"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"BCG_Facewear\data\medium\whatthefuck.paa"
-		};
+		hiddenSelections[]={"camo1"};
+		hiddenSelectionsTextures[]={"BCG_Facewear\data\medium\whatthefuck.paa"};
 	};
-	class BCG_pilot_belt: tgf_facewear_pilot_belt
-	{
+	class BCG_pilot_belt: tgf_facewear_pilot_belt {
 		scope=2;
 		scopeArsenal=2;
 		displayName="[BCG] Pilot Belt";
-		hiddenSelections[]=
-		{
-			"camo1"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"BCG_Facewear\data\pilot\camo1_co.paa"
-		};
+		hiddenSelections[]={"camo1"};
+		hiddenSelectionsTextures[]={"BCG_Facewear\data\pilot\camo1_co.paa"};
 	};
-	class BCG_MedicKama: knd_kama
-	{
+	class BCG_MedicKama: knd_kama {
 		scope=2;
 		scopeArsenal=2;
 		displayName="[BCG] Medic Kama";
-		hiddenSelections[]=
-		{
-			"camo1"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"BCG_Facewear\data\MedicKama\camo1_co.paa"
-		};
+		hiddenSelections[]={"camo1"};
+		hiddenSelectionsTextures[]={"BCG_Facewear\data\MedicKama\camo1_co.paa"};
 	};
-	class BCG_MaviKama: knd_kama
-	{
+	class BCG_MaviKama: knd_kama {
 		scope=2;
 		scopeArsenal=2;
 		displayName="[BCG] Mavi Kama";
-		hiddenSelections[]=
-		{
-			"camo1"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"BCG_Facewear\data\Customs\MaviKama.paa"
-		};
+		hiddenSelections[]={"camo1"};
+		hiddenSelectionsTextures[]={"BCG_Facewear\data\Customs\MaviKama.paa"};
 	};
-	class BCG_HaveItTheWay: G_Combat
-	{
-		scope = 2;
-		displayName = "[BCG] Crown of Vibe'trei'des";
-		model = "\BCG_Facewear\data\BK\bk_crownT.p3d";
-		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"\BCG_Facewear\data\BK\BK_Crown_co.paa"};
-		picture = "\BCG_Facewear\data\BK\bk_icon.paa";
-		ACE_Overlay = "";
+	class BCG_HaveItTheWay: G_Combat {
+		scope=2;
+		displayName="[BCG] Crown of Vibe'trei'des";
+		model="\BCG_Facewear\data\BK\bk_crownT.p3d";
+		hiddenSelections[]={"camo"};
+		hiddenSelectionsTextures[]={"\BCG_Facewear\data\BK\BK_Crown_co.paa"};
+		picture="\BCG_Facewear\data\BK\bk_icon.paa";
+		ACE_Overlay="";
 	};
 	
-	class BCG_HaveItNoWay: G_Combat
-	{
-		scope = 1;
-		displayName = "[BCG] Crown of The MLG";
-		model = "\BCG_Facewear\data\BK\bk_crownT.p3d";
-		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"\BCG_Facewear\data\BK\BK_Crown_co.paa"};
-		picture = "\BCG_Facewear\data\BK\bk_icon.paa";
-		ACE_Overlay = "\BCG_Facewear\data\BK\vibetreides.paa";
-		ACE_OverlayCracked = "\BCG_Facewear\data\BK\CombatGogglesCracked.paa";
+	class BCG_HaveItNoWay: G_Combat {
+		scope=1;
+		displayName="[BCG] Crown of The MLG";
+		model="\BCG_Facewear\data\BK\bk_crownT.p3d";
+		hiddenSelections[]={"camo"};
+		hiddenSelectionsTextures[]={"\BCG_Facewear\data\BK\BK_Crown_co.paa"};
+		picture="\BCG_Facewear\data\BK\bk_icon.paa";
+		ACE_Overlay="\BCG_Facewear\data\BK\vibetreides.paa";
+		ACE_OverlayCracked="\BCG_Facewear\data\BK\CombatGogglesCracked.paa";
 	};
-	class BCG_Duke: G_Combat
-	{
-		scope = 2;
-		displayName = "[BCG] Duke of Va'trei'des";
-		model = "\BCG_Facewear\data\BK\duke.p3d";
-		hiddenSelections[] = {"camo1","camo2"};
-		hiddenSelectionsTextures[] = {"\BCG_Facewear\data\BK\duke_camo1_ca.paa","\BCG_Facewear\data\BK\duke_camo2_co.paa"};
-		picture = "\BCG_Facewear\data\BK\bk_icon.paa";
-		ACE_Overlay = "";
+	class BCG_Duke: G_Combat {
+		scope=2;
+		displayName="[BCG] Duke of Va'trei'des";
+		model="\BCG_Facewear\data\BK\duke.p3d";
+		hiddenSelections[]={"camo1","camo2"};
+		hiddenSelectionsTextures[]={"\BCG_Facewear\data\BK\duke_camo1_ca.paa","\BCG_Facewear\data\BK\duke_camo2_co.paa"};
+		picture="\BCG_Facewear\data\BK\bk_icon.paa";
+		ACE_Overlay="";
 	};
-	class BCG_Duke_MLG: G_Combat
-	{
-		scope = 1;
-		displayName = "[BCG] Duke of MLG";
-		model = "\BCG_Facewear\data\BK\duke.p3d";
-		hiddenSelections[] = {"camo1","camo2"};
-		hiddenSelectionsTextures[] = {"\BCG_Facewear\data\BK\duke_camo1_ca.paa","\BCG_Facewear\data\BK\duke_camo2_co.paa"};
-		picture = "\BCG_Facewear\data\BK\bk_icon.paa";
-		ACE_Overlay = "\BCG_Facewear\data\BK\theduke.paa";
-		ACE_OverlayCracked = "\BCG_Facewear\data\BK\CombatGogglesCracked.paa";
+	class BCG_Duke_MLG: G_Combat {
+		scope=1;
+		displayName="[BCG] Duke of MLG";
+		model="\BCG_Facewear\data\BK\duke.p3d";
+		hiddenSelections[]={"camo1","camo2"};
+		hiddenSelectionsTextures[]={"\BCG_Facewear\data\BK\duke_camo1_ca.paa","\BCG_Facewear\data\BK\duke_camo2_co.paa"};
+		picture="\BCG_Facewear\data\BK\bk_icon.paa";
+		ACE_Overlay="\BCG_Facewear\data\BK\theduke.paa";
+		ACE_OverlayCracked="\BCG_Facewear\data\BK\CombatGogglesCracked.paa";
 	};
-	class BCG_Mommy_Hat: G_Combat
-	{
-		scope = 2;
-		displayName = "[BCG] Mother's Hat";
-		model = "\BCG_Facewear\data\mommy\mommy_hat.p3d";
-		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"\BCG_Facewear\data\mommy\camo_co.paa"};
-		ACE_Overlay = "";
+	class BCG_Mommy_Hat: G_Combat {
+		scope=2;
+		displayName="[BCG] Mother's Hat";
+		model="\BCG_Facewear\data\mommy\mommy_hat.p3d";
+		hiddenSelections[]={"camo"};
+		hiddenSelectionsTextures[]={"\BCG_Facewear\data\mommy\camo_co.paa"};
+		ACE_Overlay="";
 	};
-	
 };
 class cfgVehicles {
 	class Man;
-	class CAManBase: Man
-	{
-		class ACE_SelfActions
-		{
+	class CAManBase: Man {
+		class ACE_SelfActions {
 			class bcg_MLG {
                 displayName="Activate Gamer Mode";
                 condition="(goggles player) in [""BCG_HaveItTheWay""]";
                 statement="_player call bcg_MLG_fnc_mlgmode";
-                icon = "BCG_Facewear\data\BK\vatreicon.paa";
+                icon="BCG_Facewear\data\BK\vatreicon.paa";
 			};
 			class bcg_cringe {
                 displayName="Activate Cringe Mode";
                 condition="(goggles player) in [""BCG_HaveItNoWay""]";
                 statement="_player call bcg_MLG_fnc_cringemode";
-                icon = "BCG_Facewear\data\BK\vatreicon2.paa";
+                icon="BCG_Facewear\data\BK\vatreicon2.paa";
 			};
 			class bcg_MLG_duke {
                 displayName="Activate Gamer Mode";
                 condition="(goggles player) in [""BCG_Duke""]";
                 statement="_player call bcg_MLG_fnc_mlgmode_Duke";
-                icon = "BCG_Facewear\data\BK\vatreicon.paa";
+                icon="BCG_Facewear\data\BK\vatreicon.paa";
 			};
 			class bcg_cringe_duke {
                 displayName="Activate Cringe Mode";
                 condition="(goggles player) in [""BCG_Duke_MLG""]";
                 statement="_player call bcg_MLG_fnc_cringemode_Duke";
-                icon = "BCG_Facewear\data\BK\vatreicon2.paa";
+                icon="BCG_Facewear\data\BK\vatreicon2.paa";
 			};
 			class bcg_iPod {
 				displayName="iPod Tracks";
@@ -386,7 +314,7 @@ class cfgVehicles {
 				};
 				class iPod_Yeet {
 					displayName="Yeet's Epic Playlist";
-					condition = "(""BCG_Playlist_Yeet"" in (items _player))";
+					condition="(""BCG_Playlist_Yeet"" in (items _player))";
 					statement="";
 					icon="BCG_Facewear\data\iPod\yeet.paa";
 					class iPod_Devious {
